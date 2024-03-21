b442e6ce8bb92816b77939b1a60e09c15c86066f
helx <helx@300624.cn>
2024-03-21 20:06:52 +0800

    init project

---
+++ dist/index.js
@@ -0,0 +1,35 @@
+/*!
+  * @jslib-book/clone v1.0.0
+  * (c) 2024 
+  * @license MIT
+  */
+'use strict';
+
+function type(data) {
+  return Object.prototype.toString.call(data).slice(8, -1).toLowerCase();
+}
+
+function clone(source) {
+  var t = type(source);
+  if (t !== 'object' && t !== 'array') {
+    return source;
+  }
+  var target;
+  if (t === 'object') {
+    target = {};
+    for (var i in source) {
+      if (source.hasOwnProperty(i)) {
+        target[i] = clone(source[i]);
+      }
+    }
+  } else {
+    target = [];
+    for (var _i = 0; _i < source.length; _i++) {
+      target[_i] = clone(source[_i]);
+    }
+  }
+}
+var a = {
+  c: 1
+};
+var b = clone(a);
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import { type } from './type'
+function clone (source) {
+  const t = type(source)
+  if (t !== 'object' && t !== 'array') {
+    return source
+  }
+
+  let target
+  if (t === 'object') {
+    target = {}
+    for (let i in source) {
+      if (source.hasOwnProperty(i)) {
+        target[i] = clone(source[i])
+      }
+    }
+  } else {
+    target = []
+    for (let i = 0; i < source.length; i++) {
+      target[i] = clone(source[i])
+    }
+  }
+}
+
+let a = {
+  c: 1
+}
+
+let b = clone(a)
+
